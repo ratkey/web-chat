682d5eed7b17defb11861db67b9dd6812241b7ea
El de los tamales <158228611+QKPeludasLover@users.noreply.github.com>
2024-08-08 17:42:55 -0600

    Update messageBubble.tsx
    Oal ya edite esta parte c:

--- src/app/_components/messageBubble.tsx
+++ src/app/_components/messageBubble.tsx
@@ -1,4 +1,6 @@
-import { Avatar, Box, Stack, Tag, TagLabel, Text } from "@chakra-ui/react";
+import { Avatar, Text } from "@chakra-ui/react";
+import { createAvatar } from "@dicebear/core";
+import { funEmoji } from "@dicebear/collection";
 
 interface Props {
   message: string;
@@ -8,18 +10,21 @@ interface Props {
 }
 
 export default function MessageBubble(props: Props) {
-  const { message, image, own } = props;
+  const { message, own, userId } = props;
+
+  const avatar = createAvatar(funEmoji, {
+    seed: userId,
+    flip: !!own,
+  });
+
+  const svg = avatar.toDataUri();
+
   return (
     <>
-      <div className={`flex gap-2 ${own && "flex-row-reverse"}`}>
-        <Avatar
-          alignSelf={"start"}
-          src={image}
-          size="sm"
-          name="Segun Adebayo"
-        />
+      <div className={flex gap-2 ${own && "flex-row-reverse"}}>
+        <Avatar alignSelf={"start"} src={svg} size="sm" name="Segun Adebayo" />
         <div
-          className={`${own ? "rounded-b-xl rounded-s-xl" : "rounded-b-xl rounded-e-xl"} bg-teal-800 p-2`}
+          className={${own ? "rounded-b-xl rounded-s-xl" : "rounded-b-xl rounded-e-xl"} bg-teal-800 p-2}
         >
           <Text>{message}</Text>
         </div>
